c331d85e459b788c28f9d2632108b18ed18f979d
ghzmdr <ghzmdr@fake.mail>
2014-10-28 14:44:58 +0100

    Commit no: 7282

--- git-committer.sh
+++ git-committer.sh
@@ -2,11 +2,11 @@
 
 VAR=7282
 while [[ true ]]; do
-	for i in {1...10}; do
+	for i in {1..10}; do
 		echo $VAR > i-want-that-shirt.please
 		git add .
 		git commit -am "Commit no: $VAR"
 		((VAR++))
 	done
-	git push origin
+	git push origin master
 done
