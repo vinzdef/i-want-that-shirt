db9eeddaa403b3bceede90994d7f1e6817d50709
ghzmdr <ghzmdr@fake.mail>
2014-10-28 11:28:37 +0100

    Commit no: 5091

--- git-committer.sh
+++ git-committer.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-VAR=5082
+VAR=5091
 while [[ true ]]; do
 	echo $VAR > i-want-that-shirt.please
 	git add .
 	git commit -m "Commit no: $VAR"
 	git push origin master
-	((var++))
+	((VAR++))
 done
