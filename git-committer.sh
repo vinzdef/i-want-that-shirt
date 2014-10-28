bb363b421fc5062433a5059d60800f3627785163
ghzmdr <ghzmdr@fake.mail>
2014-10-28 15:26:22 +0100

    Commit no: 72420

--- git-committer.sh
+++ git-committer.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-VAR=40620
+VAR=72420
 while [[ true ]]; do
-	for i in {1..100}; do
+	for i in {1..1024}; do
 		echo $VAR > i-want-that-shirt.please
 		git add .
 		git commit -am "Commit no: $VAR"
