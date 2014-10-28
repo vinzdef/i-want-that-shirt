f69fb57a80c6298ef2d75dff7af4032b3e0c2bc2
ghzmdr <ghzmdr@fake.mail>
2014-10-28 14:42:17 +0100

    Commit no: 7282

--- git-committer.sh
+++ git-committer.sh
@@ -4,7 +4,7 @@ VAR=7282
 while [[ true ]]; do
 	for i in {1...10}; do
 		echo $VAR > i-want-that-shirt.please
-		git add i-want-that-shirt.please
+		git add .
 		git commit -am "Commit no: $VAR"
 		((VAR++))
 	done
